a585bdab6b290b9f9e8e281d70d02d01c08debfc
floyddroid525 <kuarora@ucsd.edu>
2021-11-15 15:30:14 -0800

    switch tab heading

--- source/js/recipe-info.js
+++ source/js/recipe-info.js
@@ -1,8 +1,19 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  const tabSelected = document.querySelector(".tab-a");
-  tabSelected.addEventListener("click", (event) => {});
+  const tabSelected = document.querySelector(".tab-a.inactive-a");
+  const otherTab = document.querySelector(".tab-a.active-a");
+
+  tabSelected.addEventListener("click", (event) => {
+    tabSelected.classList.add("active-a");
+    tabSelected.classList.remove("inactive-a");
+
+    otherTab.classList.remove("active-a");
+    otherTab.classList.add("inactive-a");
+
+    const tabPicker = tabSelected.getAttribute("data-id");
+    console.log(tabPicker);
+  });
   //   $(".tab-a").click(function () {
   //     $(".tab").removeClass("tab-active");
   //     $(".tab[data-id='" + $(this).attr("data-id") + "']").addClass("tab-active");
