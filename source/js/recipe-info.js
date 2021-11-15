9c85e966fdd8ae3d51d52abec6cfbb9a780893cf
floyddroid525 <kuarora@ucsd.edu>
2021-11-14 21:07:40 -0800

    width for mobile and tabs for recipe item

---
+++ source/js/recipe-info.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const tabSelected = document.querySelector(".tab-a");
+  tabSelected.addEventListener("click", (event) => {});
+  //   $(".tab-a").click(function () {
+  //     $(".tab").removeClass("tab-active");
+  //     $(".tab[data-id='" + $(this).attr("data-id") + "']").addClass("tab-active");
+  //     $(".tab-a").removeClass("active-a");
+  //     $(this).parent().find(".tab-a").addClass("active-a");
+  //   });
+}
