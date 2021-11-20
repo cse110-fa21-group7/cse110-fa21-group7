f6d5f5ec68751312dd3521506d3c7a5ae0237466
vrothenbergUCSD <vrothenberg@ucsd.edu>
2021-11-19 20:55:11 -0800

    Removed old function

--- source/js/update-recipe.js
+++ source/js/update-recipe.js
@@ -1,23 +1,3 @@
-function updateRecipe(id) {
-  const recipe = findRecipe(id);
-
-  const title = document.getElementById("recipeTitle");
-  const ingredient1Name = document.getElementById("ingredient1name");
-  const ingredient1Amount = document.getElementById("ingredient1amount");
-  const step1 = document.getElementById("step1");
-
-  title.value = recipe.title;
-  const ingredients = recipe.ingredients;
-  const ingredient1 = ingredients[0];
-  ingredient1Name.value = ingredient1.name;
-  ingredient1Amount.value = ingredient1.amount;
-
-  const steps = recipe.steps;
-  step1 = steps[0];
-  step1.value = step1;
-
-  createRecipe();
-}
 // update-recipe.js
 
 const MAX_INGREDIENTS = 20;
