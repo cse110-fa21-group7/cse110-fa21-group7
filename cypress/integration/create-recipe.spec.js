8983931bfa076d7fff116b588b84822201e71efc
Yalong-T <yatian@ucsd.edu>
2021-11-30 17:00:51 -0800

    add some e2e test

--- cypress/integration/create-recipe.spec.js
+++ cypress/integration/create-recipe.spec.js
@@ -1,19 +1,56 @@
 /// <reference types="cypress" />
-
+import { checkRecipeExist } from "./cook-book.spec.js";
 describe("Tests for create recipe", () => {
-  beforeEach(() => {
-    cy.visit("/source/html/create-recipe.html");
+  before(() => {
+    cy.visit("/create");
   });
 
-  it("enter the create recipe", () => {
+  it("enter create recipe page", () => {
     cy.get("#forms").contains("Create Recipe Form");
   });
 
-  it("create recipe test1", () => {
+  it("check basic input", () => {
     // create a recipe
     cy.get("#recipeTitle").type("Apple");
     cy.get("#recipeDescription").type("This is an apple recipe");
     cy.get("#recipeCost").type("10 dollars");
+
+    // submit
+
+    // cy.get(".btn-submit").click();
+
+    // // Check wheter create recipe successfully
+
+    // cy.visit("/source/html/cook-book.html");
+    // cy.get("recipe-card")
+    //   .eq(2)
+    //   .shadow()
+    //   .find(".card .card_body .card__title")
+    //   .contains("Apple");
+
+    // cy.get("recipe-card").eq(2).click();
+    // cy.get("#recipeTitle").contains("Apple");
+    // cy.get("#recipeDescription").contains("This is an apple recipe");
+    // cy.get(".cost > :nth-child(2)").contains("Cost: $10");
+    // cy.get(":nth-child(1) > .container").contains("First Ingredient 1g");
+    // cy.get(":nth-child(2) > .container").contains("Second Ingredient 2g");
+    // cy.get(":nth-child(3) > .container").contains("Third Ingredient 3g");
+    // cy.get(":nth-child(4) > .container").contains("Fourth Ingredient 4g");
+
+    // cy.get(".orderList > :nth-child(1)").contains("First Step");
+    // cy.get(".orderList > :nth-child(2)").contains("Second Step");
+    // cy.get(".orderList > :nth-child(3)").contains("Third Step");
+    // cy.get(".orderList > :nth-child(4)").contains("Fourth Step");
+  });
+  // it("check image uplad or not", () => {
+  //   const filepath = "default.png";
+  //   cy.get("#recipeImage").attachFile(filepath);
+  //   cy.get("#img-spot")
+  //     .find("img")
+  //     .should("have.attr", "src")
+  //     .should("include", "imgur");
+  // });
+  it("check ingredients functional", () => {
     // input ingredients
     cy.get(".ingredient-name input").eq(0).type("First Ingredient");
     cy.get(".ingredient-amount input").eq(0).type("1g");
@@ -25,6 +62,12 @@ describe("Tests for create recipe", () => {
     cy.get("#add-ingredient").click();
     cy.get(".ingredient-name input").eq(3).type("Fourth Ingredient");
     cy.get(".ingredient-amount input").eq(3).type("4g");
+    // check remove one ingredent
+    cy.get("#remove-ingredient").click();
+    cy.get("#ingredients").find(".ingredient").should("have.length", 3);
+    // expect("").to.have.length(3);
+  });
+  it("check steps functional", () => {
     // input steps
     cy.get(".step-sec textarea").eq(0).type("First Step");
     cy.get(".step-sec textarea").eq(1).type("Second Step");
@@ -34,30 +77,20 @@ describe("Tests for create recipe", () => {
     cy.get("#add-step").click();
     cy.get(".step-sec textarea").eq(2).type("Third Step");
     cy.get(".step-sec textarea").eq(3).type("Fourth Step");
+    // check remove one step
 
+    cy.get("#remove-step").click();
+    cy.get("#steps").find(".step-sec").should("have.length", 3);
+  });
+  it("check after submit, recipe save into local storage or not", () => {
     // submit
-    
+
     cy.get(".btn-submit").click();
-    
+    const currID = parseInt(localStorage.getItem("currID"));
+    const userRecipes = JSON.parse(localStorage.getItem("userRecipes"));
+    const recipe = userRecipes[currID];
+    expect(recipe[title]).to.eq("Apple");
 
-    // Check wheter create recipe successfully
-
-    cy.visit("/source/html/cook-book.html");
-    cy.get("recipe-card").eq(2).shadow().find(".card .card_body .card__title").contains("Apple");
-    
-    cy.get("recipe-card").eq(2).click();
-    cy.get("#recipeTitle").contains("Apple");
-    cy.get("#recipeDescription").contains("This is an apple recipe");
-    cy.get(".cost > :nth-child(2)").contains("Cost: $10");
-    cy.get(":nth-child(1) > .container").contains("First Ingredient 1g");
-    cy.get(":nth-child(2) > .container").contains("Second Ingredient 2g");
-    cy.get(":nth-child(3) > .container").contains("Third Ingredient 3g");
-    cy.get(":nth-child(4) > .container").contains("Fourth Ingredient 4g");
-
-    cy.get(".orderList > :nth-child(1)").contains("First Step");
-    cy.get(".orderList > :nth-child(2)").contains("Second Step");
-    cy.get(".orderList > :nth-child(3)").contains("Third Step");
-    cy.get(".orderList > :nth-child(4)").contains("Fourth Step");
-
+    checkRecipeExist("hello");
   });
 });
