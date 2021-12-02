606c7d81a2df719bac3e07102a53d38993e77348
Yalong-T <yatian@ucsd.edu>
2021-12-02 15:31:38 -0800

    fix card and add btn on card

--- source/js/recipe-card.js
+++ source/js/recipe-card.js
@@ -11,8 +11,7 @@ class RecipeCard extends HTMLElement {
     super();
     this.attachShadow({
       mode: "open",
-    });
-    // .innerHTML = `<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">`;
+    }).innerHTML = `<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">`;
   }
 
   /**
@@ -31,7 +30,21 @@ class RecipeCard extends HTMLElement {
     // console.log(data);
     const styleElem = document.createElement("style");
     const styles = `
-    .card {
+    .card-image {
+      height: 12rem;
+      width: 100%;
+      object-fit: cover;
+    }
+    
+    .card-title {
+      padding: 1rem;
+    }
+    
+    .card-body {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
       overflow: hidden;
       box-shadow: 0px 2px 20px #cfd8dc;
       border-radius: 0.3rem;
@@ -42,133 +55,96 @@ class RecipeCard extends HTMLElement {
       transition: transform 200ms ease-in;
       background-color: #fff;
       position: relative;
-      
-    
-    
-      /* // &__btn {
-      //   padding: 1rem;
-      //   font-family: inherit;
-      //   font-weight: bold;
-      //   font-size: 1rem;
-      //   margin: 1rem;
-      //   border: 2px solid var(--clr-primary);
-      //   background: transparent;
-      //   color: var(--clr-primary);
-      //   border-radius: var(--radius);
-      //   transition: background 200ms ease-in, color 200ms ease-in;
-      // }
-    
-      // &:hover &__btn {
-      //   background: var(--clr-primary);
-      //   color: white;
-      // } */
     }
-    
-    .card__image {
-        height: 12rem;
-        width: 100%;
-        object-fit: cover;
-      }
-    
-    .card__title {
-        padding: 1rem;
-      }
-    
-    .card__description {
-        padding: 1rem 1rem;
-      }
-    
-    .card:hover {
-        transform: scale(1.02);
-     }
-    
-     .card__body {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-     }
-    
+    .card-body:hover {
+      transform: scale(1.02);
+    }
     ul.info-row {
-        list-style: none;
-        margin: 0.5em auto;
-        padding: 0;
-        vertical-align: bottom;
+      list-style: none;
+      margin: 0.5em auto;
+      padding: 0;
+      vertical-align: bottom;
     }
     
     .info-row li {
-            display: inline-block;
-            margin-left: 1em;
-            line-height: 1em; }
+      display: inline-block;
+      margin-left: 1em;
+      line-height: 1em;
+    }
     
     .info-row li span {
       margin-left: 0.5em;
-              font-weight: 300;
-              vertical-align: middle;
-              color: #838689;
+      font-weight: 300;
+      vertical-align: middle;
+      color: #838689;
     }
     
-    .add-to-cookbook a .fa-plus {
-          background: #57abf2;
-          border-radius: 50%;
-          position: absolute;
-          top: 10.75rem;
-          right: 10%;
-          width: 2.5rem;
-          height: 2.5rem;
-          text-align: center;
-          color: white;
-          font-size: 1.3rem;
-          padding: 0.6rem 0.52rem;
+    .fa-plus {
+      display: inline-block;
+      background: #57abf2;
+      border-radius: 50%;
+      position: absolute;
+      top: 10.75rem;
+      right: 5%;
+      width: 2.3rem;
+      height: 2.3rem;
+      text-align: center;
+      padding: 0px;
+      color: white;
+      font-size: 1.5rem;
+      margin:0 auto;
+      padding-top: 5px;
+      // padding: 0.6rem 0.52rem;
     }`;
 
     styleElem.innerHTML = styles;
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
+    // const cardDiv = document.createElement("div");
+    // cardDiv.classList.add("card");
+    this.classList.add("card");
     const cardBody = document.createElement("div");
-    cardBody.classList.add("card_body");
+    this.shadowRoot.append(styleElem, cardBody);
 
-    this.shadowRoot.append(styleElem, cardDiv);
-    cardDiv.appendChild(cardBody);
+    cardBody.classList.add("card-body");
+
+    // cardDiv.appendChild(cardBody);
 
     // add img
     const img = document.createElement("img");
     img.setAttribute("src", data["image"]);
     img.setAttribute("alt", data["title"]);
-    img.classList.add("card__image");
+    img.classList.add("card-image");
     cardBody.appendChild(img);
     // test add btn
-    // const addBtn = document.createElement("div");
-    // addBtn.classList.add("add-to-cookbook");
-    // const ai = document.createElement("a");
-    // ai.href = "#";
-    // const ii = document.createElement("i");
-    // ii.classList.add("fa");
-    // ii.classList.add("fa-plus");
-    // ai.appendChild(ii);
-    // addBtn.appendChild(ai);
-    // cardBody.appendChild(addBtn);
-    //   <div class="add-to-cookbook">
-    //   <a href="#"><i class="fa fa-plus"></i></a>
-    // </div>
+    const addBtn = document.createElement("div");
+    addBtn.classList.add("add-to-cookbook");
+
+    const ii = document.createElement("i");
+    ii.classList.add("fa");
+    ii.ariaHidden = "true";
+    if (this.page === "cookbook") {
+      ii.classList.add("fa-trash");
+    } else {
+      ii.classList.add("fa-plus");
+    }
+    addBtn.appendChild(ii);
+    cardBody.appendChild(addBtn);
+
     // add title
     const title = document.createElement("h2");
     title.textContent = data["title"];
-    title.classList.add("card__title");
+    title.classList.add("card-title");
     cardBody.appendChild(title);
 
     if (this.page == "curatedList" || this.page == "cookbook") {
       // add info list
       const ul = document.createElement("ul");
       ul.classList.add("info-row");
-      //
-
-      // li.append(i, span)
-      ul.appendChild(addList("38 min"));
-      // add kcal
-      ul.appendChild(addList("318 kcal"));
+      // append time
+      ul.appendChild(addList("fa-clock-o", `${data["time"]} mins`));
+      // append servings
+      ul.appendChild(addList("fa-user", data["servings"]));
       // add total cost
-      ul.appendChild(addList(`$ ${data["totalCost"]}`));
+      ul.appendChild(addList("cost", `$ ${data["totalCost"]}`));
       cardBody.appendChild(ul);
     }
   }
@@ -176,10 +152,11 @@ class RecipeCard extends HTMLElement {
 customElements.define("recipe-card", RecipeCard);
 /**
  * Get recipes from localStorage
+ * @param {string} classNmae
  * @param {string} value
  * @return {HTMLElement}
  */
-function addList(value) {
+function addList(classNmae, value) {
   // symbol does not work need to ask Kunal how to fix
   // const i = document.createElement('i');
   // const span = document.createElement('span');
@@ -189,7 +166,7 @@ function addList(value) {
   const i = document.createElement("i");
 
   i.classList.add("fa");
-  i.classList.add("fa-clock-o");
+  i.classList.add(classNmae);
   i.ariaHidden = "true";
 
   const span = document.createElement("span");
