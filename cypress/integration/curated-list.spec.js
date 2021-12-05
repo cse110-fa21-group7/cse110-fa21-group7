4e8675c327e76acc3192e0f4825e5fecd564a10a
FAN-666666 <77092886+FAN-666666@users.noreply.github.com>
2021-12-04 19:47:26 -0800

    curated list test

--- cypress/integration/curated-list.spec.js
+++ cypress/integration/curated-list.spec.js
@@ -5,11 +5,32 @@ describe("Tests for curated list", () => {
 
     cy.saveLocalStorage();
   });
+
+  beforeEach(() => {
+      cy.restoreLocalStorage();
+    });
+    afterEach(() => {
+      cy.saveLocalStorage();
+    });
+
+  it("check number of recipes", () => {
+    cy.get("recipe-card").should("have.length", 4);
+  });
+
+
   it("check add button", () => {
-    cy.get(".card")
-      .eq(2)
+    // add recipe
+    for(let i=2;i<4;i++){
+      cy.get(".card")
+      .eq(i)
       .shadow()
       .find(".card-body .add-to-cookbook i")
       .click();
+    }
+
+    // after adding recipe to cookbook, check cookbook
+    cy.visit("/cookbook");
+    cy.get("recipe-card").should("have.length", 4);
+    
   });
 });
