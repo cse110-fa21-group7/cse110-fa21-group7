004ed93cf4d9c7b356e225501efa27045f5835c8
Kimberly <j9liu@ucsd.edu>
2021-12-06 21:31:38 -0800

    revert youjia's change

--- source/test/create-recipe.test.js
+++
@@ -1,8 +0,0 @@
-// import { showError, showSuccess } from "../js/create-recipe";
-
-// // const sum = require("../js/create-recipe");
-
-// test("show error message", () => {
-//   expect(showError()).toBe(3);
-
-// });
