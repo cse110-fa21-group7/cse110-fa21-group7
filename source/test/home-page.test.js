fc9032a8bf11f320d91e109aae85ae6a417767e1
Yalong-T <yatian@ucsd.edu>
2021-12-09 10:33:15 -0800

    fix eslint

--- source/test/home-page.test.js
+++ source/test/home-page.test.js
@@ -7,29 +7,32 @@ describe("test getRandom", () => {
     const resultArray = getRandom(36);
     expect(resultArray.length).toBe(12);
   });
-  
+
   test("getRandom return non-duplicated items", () => {
-    let exist = [];
+    const exist = [];
     const resultArray = getRandom(40);
     resultArray.forEach((e) => {
       expect(exist[e]).toBe(undefined);
       exist[e] = true;
     });
-  })
-})
+  });
+});
 
 describe("test dictToArr", () => {
-  let dict = {"1": "hello", "2":"world"};
-  let arr = [["1", "hello"], ["2", "world"]];
+  const dict = { 1: "hello", 2: "world" };
+  const arr = [
+    ["1", "hello"],
+    ["2", "world"],
+  ];
   test("test convert dictionary to array function", () => {
-      expect(dictToArr(dict)).toStrictEqual(arr);
+    expect(dictToArr(dict)).toStrictEqual(arr);
   });
 });
 
 describe("test createCuratedlist function", () => {
   test("test if the length is correct", () => {
-    let tempStored = {}
-    for(let i = 1; i <= 31; i++) {
+    const tempStored = {};
+    for (let i = 1; i <= 31; i++) {
       tempStored[i] = i;
     }
     window.localStorage.setItem("storedRecipes", JSON.stringify(tempStored));
